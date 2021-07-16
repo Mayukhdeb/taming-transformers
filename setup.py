a0941c1cdf27c9b9b4f866dfdf4e38568baf95e4
mayukhdeb <mayukhmainak2000@gmail.com>
2021-07-16 20:01:40 +0530

    add requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='taming-transformers',
     version='0.0.1-eden',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
     include_package_data=True,
+    install_requires= required,
     install_requires=[
         'torch',
         'numpy',
