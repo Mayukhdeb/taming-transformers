023fc74260c4991a5b70a08e1039955c2fcbac74
mayukh <53133634+Mayukhdeb@users.noreply.github.com>
2021-07-16 18:34:09 +0530

    setup: include package data

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'torch',
         'numpy',
