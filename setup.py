a3f8c1d2e73260e828259ad6c91dcdc91354cdc8
mayukhdeb <mayukhmainak2000@gmail.com>
2021-07-16 20:10:48 +0530

    fix install error

--- setup.py
+++ setup.py
@@ -10,9 +10,4 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires= required,
-    install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
-    ],
 )
