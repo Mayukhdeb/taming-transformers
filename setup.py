3564845763377c3f7085b6b27ecd33deb040cdbf
mayukhdeb <mayukhmainak2000@gmail.com>
2021-07-16 19:23:47 +0530

    tweak version name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taming-transformers',
-    version='0.0.1',
+    version='0.0.1-eden',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
     include_package_data=True,
